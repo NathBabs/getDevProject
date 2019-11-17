6524b36e87d29d0677760140192231e95676da3e
Nathaniel <nathanielbabalola1@gmail.com>
2019-11-17 12:47:21 +0100

    Added the function to write result to a .txt file

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const csv = require('csv-parser');
 const fs = require('fs');
 let counter = 0;
+let wordsArray = [];
 
 fs.createReadStream('./assets/768hhT.csv')
 //automatically csv-parser uses the first row as headers(columns), so to disable that you have to set headers to false
@@ -8,11 +9,13 @@ fs.createReadStream('./assets/768hhT.csv')
     .on('data', (row) => {
         //this line converts the row object to an array
         const rowArray = Object.keys(row);
-        console.log(row[counter]);
+        wordsArray.push(row[counter]);
         counter++;
+        console.log(row[counter]);
     })
     .on('end', () => {
-        console.log('CSV file succesfully processed');
+        fs.writeFileSync('./assets/results.txt', wordsArray);
+        console.log('The CSV file has succesfully been processed');
     });
 
    /*  console.log(csvString.detect('./assets/768hhT.csv')); */
